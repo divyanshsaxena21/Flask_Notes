bd0aaed8cd1f3086832fc97c4658c3839d6d0887
Divyansh <divyansh_saxena@yahoo.com>
2024-11-01 16:33:22 +0530

    lec 3

---
+++ Lecture_3/employees.py
@@ -0,0 +1,28 @@
+employees_data = {
+    1:{
+        "name":"Michael",
+        "age":42,
+        "position":"Manager"
+    },
+    2:{
+        "name":"Dwight",
+        "age":35,
+        "position":"Salesperson"
+    },
+    3:{
+        "name":"Pam",
+        "age":23,
+        "position":"Receptionsit"
+    },
+    4:{
+        "name":"Jim",
+        "age":31,
+        "position":"Salesperson"
+    },
+    5:{
+        "name":"Josh",
+        "age":40,
+        "position":"Manager"
+    }
+
+}
